7de36c9df7a1711fff45d2cc2cac2dfa9c9b8280
Mawadda Alshukaili <moodalshukaili99@gmail.com>
2026-04-07 15:10:54 +0400

    Solving Task17

---
+++ src/Task17.java
@@ -0,0 +1,17 @@
+public class Task17 {
+    public static void main(String[] args) {
+        System.out.println(inOrder(1, 2, 3, false));
+
+
+    }
+
+    public static boolean inOrder(int a, int b, int c, boolean bOk) {
+
+        if (bOk) {
+            return c > b;
+        } else {
+            return b > a && c > b;
+
+        }
+    }
+}
