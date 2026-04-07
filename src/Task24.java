6cbf86ce7a7db17520c482552d0b31d40eded206
Mawadda Alshukaili <moodalshukaili99@gmail.com>
2026-04-07 16:26:12 +0400

    Solving Task24

---
+++ src/Task24.java
@@ -0,0 +1,13 @@
+public class Task24  {
+    public static void main(String[] args) {
+        System.out.println(less20(18));
+    }
+    public static  boolean less20(int n){
+    if ((n % 20 == 18) || (n % 20 == 19)){
+        return true;
+    }else{
+        return false;
+    }
+
+    }
+}
