7cc132836e2101c87fcd1869bb203b551d4c187c
Mawadda Alshukaili <moodalshukaili99@gmail.com>
2026-04-07 15:17:36 +0400

    Solving Task18

---
+++ src/Task18.java
@@ -0,0 +1,17 @@
+public class Task18 {
+    public static void main(String[] args) {
+        System.out.println(lessBy10(1, 7, 11));
+
+
+    }
+
+    public static boolean lessBy10(int a, int b, int c){
+        if ((Math.abs(a-b) >= 10) || (Math.abs(a-c) >= 10) || (Math.abs(b-c) >= 10)) {
+            return true;
+        } else {
+            return false;
+
+        }
+    }
+}
+
